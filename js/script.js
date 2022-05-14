4263d8417daea9be7240d9a61ffb93f704e04849
olegpl1993 <79967092+olegpl1993@users.noreply.github.com>
2022-05-15 02:42:06 +0300

    Добавил инструмент круг

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 let canvas = document.getElementById("c1");
 let ctx = canvas.getContext("2d");
 
+let pi = Math.PI; //pi
 let myColor = "black"; //цвет
 let brStyle = "round"; //тип кисти
 let brush = 5; //размер кисти
@@ -15,7 +16,7 @@ document.getElementById("color").oninput = function () {
 }
 
 
-//выбор кисти
+//выбор кисти, инструмента
 brush5x5.onclick = function () {
     brush = 5;
     brStyle = "square";
@@ -56,32 +57,56 @@ brushR25.onclick = function () {
     brush = 25;
     brStyle = "round";
 }
+circle.onclick = function () {
+    brStyle = "circle";
+}
 
 //очистить лист
 clear.onclick = function () {
     ctx.clearRect(0, 0, 1750, 900);
-    console.log(brStyle);
 }
 
 
-//функция рисования
+
 canvas.onmousedown = function (event) {
-    let x = event.offsetX;
-    let y = event.offsetY;
-    ctx.moveTo(x, y);
-    ctx.lineTo(x, y);
-    ctx.strokeStyle = myColor;
-    ctx.lineWidth = brush;
-    ctx.lineCap = brStyle;
-    ctx.stroke();
-    ctx.beginPath();
+    //функция рисования кистью
+    if (brStyle == "round" || brStyle == "square") {
+        ctx.beginPath()
+        let x = event.offsetX;
+        let y = event.offsetY;
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y);
+        ctx.strokeStyle = myColor;
+        ctx.lineWidth = brush;
+        ctx.lineCap = brStyle;
+        ctx.stroke();
+    }
+    //функция рисования круга
+    if (brStyle == "circle") {
+        ctx.beginPath();
+        var firstx = event.offsetX;
+        var firsty = event.offsetY;
+    }
     canvas.onmousemove = function (event) {
+        //функция рисования кистью 
+        if (brStyle == "round" || brStyle == "square") {
             let x = event.offsetX;
             let y = event.offsetY;
             ctx.moveTo(x, y);
             ctx.lineTo(x, y);
             ctx.stroke();
             ctx.beginPath();
+        }
+        //функция рисования круга
+        if (brStyle == "circle") {
+            var x = event.offsetX;
+            var y = event.offsetY;
+            ctx.lineWidth = 10;
+            ctx.strokeStyle = myColor;
+            var radius = Math.pow(Math.pow(x - firstx, 2) + Math.pow(y - firsty, 2), 0.5);
+            ctx.arc(firstx, firsty, radius, 0, 2 * pi, false);
+            ctx.stroke();
+        }
     }
     canvas.onmouseup = function () {
         canvas.onmousemove = null;
