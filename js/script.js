01a5f3fdac8ed8cc1be2fcdee211e09923fafe3e
olegpl1993 <79967092+olegpl1993@users.noreply.github.com>
2022-05-19 02:13:06 +0300

    add new tools

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 'use strict'
 
-
 let canvas = document.getElementById("c1");
 let ctx = canvas.getContext("2d");
 
 let pi = Math.PI; //pi
 let myColor = "black"; //цвет
-let brStyle = "round"; //тип кисти
+let brStyle = "round"; //тип кисти или инструмента
 let brush = 5; //размер кисти
-
 
 //выбор цвета
 document.getElementById("color").oninput = function () {
     myColor = this.value;
 }
-
 
 //выбор кисти, инструмента
 brush5x5.onclick = function () {
@@ -59,6 +56,31 @@ brushR25.onclick = function () {
 }
 circle.onclick = function () {
     brStyle = "circle";
+    brush = 15;
+}
+line5.onclick = function () {
+    brStyle = "line";
+    brush = 5;
+}
+line7.onclick = function () {
+    brStyle = "line";
+    brush = 7;
+}
+line10.onclick = function () {
+    brStyle = "line";
+    brush = 10;
+}
+line15.onclick = function () {
+    brStyle = "line";
+    brush = 15;
+}
+line20.onclick = function () {
+    brStyle = "line";
+    brush = 20;
+}
+line25.onclick = function () {
+    brStyle = "line";
+    brush = 25;
 }
 
 //очистить лист
@@ -66,29 +88,44 @@ clear.onclick = function () {
     ctx.clearRect(0, 0, 1750, 900);
 }
 
-
-
+//при нажатии ЛКМ----------------------------------------
 canvas.onmousedown = function (event) {
-    //функция рисования кистью
+    // рисования кистью 
     if (brStyle == "round" || brStyle == "square") {
-        ctx.beginPath()
+        ctx.beginPath() //начало новой фигуры
         let x = event.offsetX;
         let y = event.offsetY;
-        ctx.moveTo(x, y);
-        ctx.lineTo(x, y);
+        ctx.moveTo(x, y); //установка начальных координат
+        ctx.lineTo(x, y); //установка координат для рисования
         ctx.strokeStyle = myColor;
         ctx.lineWidth = brush;
         ctx.lineCap = brStyle;
-        ctx.stroke();
+        ctx.stroke(); //отрисовка фигуры
     }
-    //функция рисования круга
+    // рисования круга
     if (brStyle == "circle") {
         ctx.beginPath();
         var firstx = event.offsetX;
         var firsty = event.offsetY;
+        ctx.lineWidth = brush;
+        ctx.strokeStyle = myColor;
+        ctx.lineCap = "round";
     }
+    //рисование линий
+    if (brStyle == "line") {
+        ctx.beginPath();
+        var x = event.offsetX;
+        var y = event.offsetY;
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y);
+        ctx.strokeStyle = myColor;
+        ctx.lineWidth = brush;
+        ctx.lineCap = "round";
+    }
+
+    //при движении мышки с зажатым ЛКМ------------------------------
     canvas.onmousemove = function (event) {
-        //функция рисования кистью 
+        //рисования кистью 
         if (brStyle == "round" || brStyle == "square") {
             let x = event.offsetX;
             let y = event.offsetY;
@@ -97,19 +134,32 @@ canvas.onmousedown = function (event) {
             ctx.stroke();
             ctx.beginPath();
         }
-        //функция рисования круга
+        //рисования круга
         if (brStyle == "circle") {
             var x = event.offsetX;
             var y = event.offsetY;
-            ctx.lineWidth = 10;
-            ctx.strokeStyle = myColor;
             var radius = Math.pow(Math.pow(x - firstx, 2) + Math.pow(y - firsty, 2), 0.5);
             ctx.arc(firstx, firsty, radius, 0, 2 * pi, false);
             ctx.stroke();
         }
     }
-    canvas.onmouseup = function () {
-        canvas.onmousemove = null;
+
+    //при отпуске ЛКМ--------------------------------------
+    canvas.onmouseup = function (event) {
+        //рисование линий
+        if (brStyle == "line") {
+            var x = event.offsetX;
+            var y = event.offsetY;
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        }
+        canvas.onmousemove = null; //отмена отслеживания координат мышки
     }
 }
+
+
+
+
+
+
 
